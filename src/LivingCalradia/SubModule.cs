dcc1b8ed5ea6b9ac06dec250b530699e159b26e4
Erdem <el23ii@leeds.ac.uk>
2026-08-23 02:23:00 +0100

    Prototype 0.01 - Loading the module with and xml file. xml file needs to have the working version for the game to not give errors when starting

---
+++ src/LivingCalradia/SubModule.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace LivingCalradia
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            InformationManager.DisplayMessage(
+                new InformationMessage("Living Calradia loaded.")
+            );
+        }
+    }
+}
